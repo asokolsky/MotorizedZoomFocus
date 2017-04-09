6b8243a1a0d9067880a3c5351057a750d3b9955e
Alex Sokolsky <asokolsky@yahoo.com>
2017-04-08 17:50:32 -0700

    Storing strings in flash now

--- Trace.h
+++ Trace.h
@@ -9,15 +9,19 @@
 #endif
 
 #ifdef DEBUG
-  #define DEBUG_PRINT(x)    Serial.print(x)
+  #define DEBUG_PRINT(x)    Serial.print(F(x))
+  #define DEBUG_PRNT(x)    Serial.print(x)
   #define DEBUG_PRINTDEC(x) Serial.print(x, DEC)
   #define DEBUG_PRINTHEX(x) Serial.print(x, HEX)
-  #define DEBUG_PRINTLN(x)  Serial.println(x)
+  #define DEBUG_PRINTLN(x)  Serial.println(F(x))
+  #define DEBUG_PRNTLN(x)  Serial.println(x)
 #else
   #define DEBUG_PRINT(x)
+  #define DEBUG_PRNT(x)
   #define DEBUG_PRINTDEC(x)
   #define DEBUG_PRINTHEX(x)
   #define DEBUG_PRINTLN(x)
+  #define DEBUG_PRNTLN(x)
 #endif 
 
 #endif //TRACE_h
